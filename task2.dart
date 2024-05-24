2d2f2c220dbe61b7b4ccb44a2f64b166cb93d59c
ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-05-24 23:58:09 +0500

    Add files via upload

---
+++ task2.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num1 = 12;
+  int num2 = 91;
+  int num3 = num1 + num2;
+  int num4 = num1 - num2;
+  int num5 = num1 * num2;
+  print(num3);
+  print(num4);
+  print(num5);
+}
